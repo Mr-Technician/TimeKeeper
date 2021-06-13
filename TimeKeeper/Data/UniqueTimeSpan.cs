04ebc2ae7b7e39ef6297000c38bc77ac1c8fc817
Riley Nielsen <thatr2d2cat@gmail.com>
2021-06-13 13:47:59 -0500

    Added default carousel.

---
+++ TimeKeeper/Data/UniqueTimeSpan.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimeKeeper.Data
+{
+    public class UniqueTimeSpan
+    {
+        public Guid Guid { get; set; }
+        public TimeSpan Start { get; set; }
+        public TimeSpan End { get; set; }
+
+        public UniqueTimeSpan() => Guid = Guid.NewGuid();
+    }
+}
--- TimeKeeper/Program.cs
+++ TimeKeeper/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -21,6 +22,8 @@ namespace TimeKeeper
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddMudServices();
+
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
