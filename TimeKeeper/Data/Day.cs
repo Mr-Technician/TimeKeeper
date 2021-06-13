2a7510798891e8689baa8c18092d5324ae94973f
Riley Nielsen <thatr2d2cat@gmail.com>
2021-06-13 13:47:59 -0500

    Added default carousel.

--- TimeKeeper/Data/Day.cs
+++ TimeKeeper/Data/Day.cs
@@ -13,11 +13,16 @@ namespace TimeKeeper.Data
         /// <summary>
         /// Stores the times worked within a day
         /// </summary>
-        public IList<TimeSpan> Times { get; set; }
+        public IList<UniqueTimeSpan> Times { get; set; }
 
         /// <summary>
         /// The day the times are logged for
         /// </summary>
         public DateTime Date { get; set; }
+
+        public Day(DateTime date)
+        {
+            Date = date;
+        }
     }
 }
